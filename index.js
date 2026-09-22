e61e7517128306bc486224110d0375e40956330b
Ashlyn Knox <ashx3s@tuta.io>
2026-09-22 15:51:20 -0600

    keydown classlist javascript example

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const boxes = {
+  ArrowUp: { el: document.getElementById("box-up"), index: 0 },
+  ArrowDown: { el: document.getElementById("box-down"), index: 0 },
+};
+
+window.addEventListener("keydown", (e) => {
+  const entry = boxes[e.key];
+  console.log("event: ", e, "Entry", entry);
+  // remove a color
+  entry.el.classList.remove("bg-blue-500");
+  // add a colour
+  entry.el.classList.add("bg-red-500");
+});
